b4c5ddc04aa5bf9c41ff3e3336cdc946a37b9c81
yincaicai <yincaiyun2021@163.com>
2023-07-08 22:03:32 +0800

    fix/modified

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,38 @@
 // 添加自定义别名
 const path=require('path')
+const {whenProd,getPlugin,pluginByName}=require('@craco/craco')
 module.exports={
 	webpack:{
 		alias:{
 			// eslint-disable-next-line no-undef
 			'@':path.join(__dirname,'src')
+		},
+		//配置CDN
+		cnfigure:(webpackConfig)=>{
+			let cdn={
+				js:[],
+				css:[]
+			}
+			// 只有生产环境下配置--开发环境下的cdn是可以直接用的
+			whenProd(()=>{
+				webpackConfig.externals={
+					react:"React",
+					'react-dom':'ReactDOM'
+				}
+				cdn={
+					js:['https://cdn.bootcdn.net/ajax/libs/react/17.0.2/umd/react.production.min.js','https://cdn.bootcn.net/ajax/libs/react-dom/17.0.2/umd/react-dom.production.min.js'],
+					css:[]
+				}
+			})
+			const {isFound,match}=getPlugin(
+				webpackConfig,
+				pluginByName('HtmlWebpackPlugin')
+			)
+			if(isFound){
+				// 找到了HtmlWebpackPlugin插件
+				match.userOptions.cdn=cdn
+			}
+			return webpackConfig
 		}
 	}
 }
--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import history from "./utils/history";
-import Login from "@/pages/Login";
+
 import LayoutView from "@/pages/Layout";
 import AuthComponent from "./components/AuthComponent";
-import Home from "./pages/Home/index";
-import Article from "./pages/Article";
-import Publish from "./pages/Publish";
 import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
+import { lazy, Suspense } from "react";
+const Login = lazy(() => import("@/pages/Login"));
+const Home = lazy(() => import("./pages/Home/index"));
+const Article = lazy(() => import("./pages/Article/index"));
+const Publish = lazy(() => import("./pages/Publish/index"));
 
 function App() {
   return (
     // <BrowserRouter>
     <HistoryRouter history={history}>
-      <div style={{margin:'0px'}}>
-        <Routes>
-          <Route path="/login" element={<Login></Login>}></Route>
-          {/* <Route path='/' element={<Layout></Layout>}></Route> */}
-          {/* element里面不能写死 */}
-          <Route
-            path="/"
-            element={
-              <AuthComponent>
-                <LayoutView></LayoutView>
-              </AuthComponent>
-            }
-          >
-            <Route index element={<Home />}></Route>
-            <Route path="/article" element={<Article />}></Route>
-            <Route path="/publish" element={<Publish />}></Route>
-          </Route>
-        </Routes>
-      </div>
+      <Suspense fallback={
+		<div style={{textAlign:'center',marginTop:200}}>loading....</div>
+	  }>
+        <div style={{ margin: "0px" }}>
+          <Routes>
+            <Route path="/login" element={<Login></Login>}></Route>
+            {/* <Route path='/' element={<Layout></Layout>}></Route> */}
+            {/* element里面不能写死 */}
+            <Route
+              path="/"
+              element={
+                <AuthComponent>
+                  <LayoutView></LayoutView>
+                </AuthComponent>
+              }
+            >
+              <Route index element={<Home />}></Route>
+              <Route path="/article" element={<Article />}></Route>
+              <Route path="/publish" element={<Publish />}></Route>
+            </Route>
+          </Routes>
+        </div>
+      </Suspense>
     </HistoryRouter>
 
     // </BrowserRouter>
